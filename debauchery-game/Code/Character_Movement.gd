71091cc5a9901f96d8f244608441143a2b59481c
FlameDrone <MaxGertenbach@gmx.de>
2024-10-06 04:42:05 +0200

    Improved the Coding Image
    Added an underscore, to define that not used variables get an underscore in front of them.

--- debauchery-game/Code/Character_Movement.gd
+++ debauchery-game/Code/Character_Movement.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	
 	# Handles Left and Right
 	var left_right: float = Input.get_axis("ui_left", "ui_right")
