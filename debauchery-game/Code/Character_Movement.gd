dd3a5e570cd2c94c6d3f7083c64d93f0c4050140
FlameDrone <MaxGertenbach@gmx.de>
2024-10-06 04:39:04 +0200

    Basic Godot Init
    Pretty much just added everything for Godot, plus a Test Scene

---
+++ debauchery-game/Code/Character_Movement.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+#	This is the Code for the Movement of the Player Character
+#	Its only Prototypic, so open to changes
+#	[Currently only working with Arrowkeys]
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	
+	# Handles Left and Right
+	var left_right: float = Input.get_axis("ui_left", "ui_right")
+	if left_right:
+		velocity.x = left_right * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Handles Up and Down
+	var up_down: float = Input.get_axis("ui_up", "ui_down")
+	if up_down:
+		velocity.y = up_down * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+
+	move_and_slide()
